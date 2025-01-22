1965b3e27b95598e9770064a7bb92ebc2947d541
NikhilDS-Rice <nd66@rice.edu>
2025-01-21 18:46:12 -0600

    MongoDB conn done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,18 @@
 import express from "express";
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth-route.js"
+import { connectDB } from "./lib/db.js";
 
+
+dotenv.config()
 const app = express();
 
+const PORT = process.env.PORT
+
+app.use("/api/auth", authRoutes)
+
 app.listen(5001, () => {
-    console.log("Server is in 5001")
+    console.log("Server is on " + PORT)
+    connectDB();
 });
 
