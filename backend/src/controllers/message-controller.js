6d19c0f42ab3e1c0761fa3dbb332da2b7ed10d19
NikhilDS-Rice <nd66@rice.edu>
2025-01-23 17:11:34 -0600

    added side far users

---
+++ backend/src/controllers/message-controller.js
@@ -0,0 +1,13 @@
+
+
+
+export const getUsersForSidebar = async (req,res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-passsword");
+        
+    } catch (error) {
+        console.error("Error in getUsersForSidebar", error.message);
+        res.status(500).json({error:"Internal Server error"});
+    }
+}
