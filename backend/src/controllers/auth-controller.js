78ea841dfa32ebb2c4e96bd98b59c2bfe4ed3d9b
NikhilDS-Rice <nd66@rice.edu>
2025-01-22 22:49:26 -0600

    login,logout,update funcs added

--- backend/src/controllers/auth-controller.js
+++ backend/src/controllers/auth-controller.js
@@ -1,32 +1,38 @@
 import { generateToken } from "../lib/utils.js"
 import User from "../models/user-model.js"
 import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js"
 
 export const signup = async (req,res)=>
     {
         const {fullName, email, password} = req.body
         try {
+
+            if(!fullName || !email || !password){
+                return res.status(400).json({message: "All fields are required"});
+            }
+
             if (password.length < 8) {
                 return res.status(400).json({message: "Password must be at least 8 characters"});
             }
 
-            const user = await User.findOne({email})
+            const user = await User.findOne({email});
 
             if (user) return res.status(400).json({message: "Email already exists"});
 
             // hash passwd
             const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(password, salt)
+            const hashedPassword = await bcrypt.hash(password, salt);
 
             const newUser = new User({
                 fullName: fullName,
                 email: email,
                 password: hashedPassword
-            })
+            });
 
             if(newUser){
                 // generate token
-                generateToken(newUser._id, res)
+                generateToken(newUser._id, res);
                 await newUser.save();
 
                 res.status(201).json({
@@ -44,18 +50,71 @@ export const signup = async (req,res)=>
 
 
         } catch (error) {
-            console.log("Eroor in sighnup", error.message);
-            res.status(500).json({message: "Server error"});
+            console.log("Error in signnup", error.message);
+            res.status(500).json({message: "Server error "});
 
         }
     }
 
-export const login = (req,res)=>
+export const login = async(req,res)=>
     {
-        res.send("login route")
+        const {email, password} = req.body;
+        try {
+            const user = await User.findOne({email});
+
+            if(!user){
+                return res.status(400).json({message:"Invalid credentials"});
+            }
+
+            const isPassCorrect = await bcrypt.compare(password, user.password);
+
+            if(!isPassCorrect){
+                return res.status(400).json({message:"Invalid credentials"});
+            }
+
+            generateToken(user._id, res);
+
+            res.status(200).json({
+                _id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                profilePic: user.profilePic,
+            })
+        } catch (error) {
+            console.log("Error in login", error.message);
+            res.status(500).json({message: "Internal Server Error"})
+        }
     }
 
 export const logout = (req,res)=>
     {
-        res.send("logout route")
+        try {
+            res.cookie("jwt", "", {maxAge:0});
+            res.status(200).json({message:"Logout Successful"})
+
+        } catch (error) {
+            console.log("Error in logout", error.message);
+            res.status(500).json({message: "Internal Server Error"})
+
+        }
+    }
+
+    export const updateProfile = async(req,res) =>{
+        try {
+            const {profilePic} = req.body;
+            const userId = req.user._id;
+
+            if (!profilePic){
+                return res.status(400).json({message:"Profile pic is required"});
+            }
+
+            const uploadResponse = await cloudinary.uploader.upload(profilePic);
+            const updatedUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url}, {new:true});
+            
+            res.status(200).json(updatedUser);
+        
+        } catch (error) {
+            console.log("error in update profile", error);
+            res.status(500).json({message:"Internal server error"});
+        }
     }
--- backend/src/models/user-model.js
+++ backend/src/models/user-model.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema(
         required: true,
         unique: true,
     },
-    fullNamse:{
+    fullName:{
         type: String,
         required: true,
     },
